184e64b5c4040c56d4308bd004f470a09dcc4671
alvaro <almiavicas@gmail.com>
2020-02-21 15:45:59 -0400

    added dummy user

--- trackerApp/models.py
+++ trackerApp/models.py
@@ -9,6 +9,12 @@ class User(models.Model):
     password = models.CharField(max_length=16)
     position = models.CharField(max_length=20)
 
+    def __str__(self):
+        return ('\nNombre: ' + self.first_name + ' ' + self.last_name +
+            '\nCorreo: ' + self.email +
+            '\nPosicion: ' + self.position
+            )
+
 class Activity(models.Model):
     title = models.CharField(max_length=80)
     start = models.DateField()
