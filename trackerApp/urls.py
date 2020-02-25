e93b8e94e851c643cf0ba4700d9ffaa53ba47b72
JRBarreraM <mrjrbarreram@gmail.com>
2020-02-24 23:06:25 -0400

    profile 1, logout, home

--- trackerApp/urls.py
+++ trackerApp/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include, re_path
 
 from .views import IndexView, RedirectView, LoginView, RegisterView, ProfileView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='login/'), name='redirect'),
-    path('index/', IndexView.as_view(), name='index'),
+    re_path(r'^index/$', IndexView.as_view(), name='index'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
