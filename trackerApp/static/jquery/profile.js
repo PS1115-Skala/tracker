8af78940439f5e033213282a187090f6d4c18258
David Segura <dsegura95@hotmail.com>
2020-02-27 19:28:49 -0400

    Añadida la caracteristica del boton editar y del boton guardar

---
+++ trackerApp/static/jquery/profile.js
@@ -0,0 +1,13 @@
+function edit() {
+    document.getElementById("exampleInputEmail1").readOnly = false;
+    document.getElementById("exampleInputPassword1").readOnly = false;
+    document.getElementById("button-gender").disabled = false;
+    document.getElementById("button-save").hidden = false;
+}
+
+function save(){
+    document.getElementById("exampleInputEmail1").readOnly = true;
+    document.getElementById("exampleInputPassword1").readOnly = true;
+    document.getElementById("button-gender").disabled = true;
+    document.getElementById("button-save").hidden = true;
+}
