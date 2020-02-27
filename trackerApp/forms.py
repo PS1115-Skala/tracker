9ee735b56fa55cd0b05fe99515d2dce988904066
alvaro <almiavicas@gmail.com>
2020-02-27 19:29:13 -0400

    implemented widget attr

--- trackerApp/forms.py
+++ trackerApp/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 
+
 class LoginForm(forms.Form):
     your_email = forms.EmailField(label='Correo Electrónico', max_length=100)
     your_pass = forms.CharField(label='Contraseña', max_length=16)
 
+
 class RegisterForm(forms.Form):
-    your_name = forms.CharField(label='Nombre', max_length=100)
-    your_last_name = forms.CharField(label='Apellido', max_length=100)
-    your_email = forms.EmailField(label='Correo Electrónico', max_length=100)
-    your_pass = forms.CharField(label='Contraseña', max_length=16)
-    your_position = forms.CharField(label='Cargo', max_length=20)
+    your_name = forms.CharField(label='Nombre', max_length=100, 
+        widget=forms.TextInput(attrs={'class': 'expand'}))
+
+    your_last_name = forms.CharField(label='Apellido', max_length=100,
+        widget=forms.TextInput(attrs={'class': 'expand'}))
+
+    your_email = forms.EmailField(label='Correo Electrónico', max_length=100,
+        widget=forms.TextInput(attrs={'class': 'expand'}))
+
+    your_pass = forms.CharField(label='Contraseña', max_length=16,
+        widget=forms.TextInput(attrs={'class': 'expand', 'type': 'password'}))
+
+    your_position = forms.CharField(label='Cargo', max_length=20,
+        widget=forms.TextInput(attrs={'class': 'expand'}))
