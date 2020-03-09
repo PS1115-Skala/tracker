41f0d796311f33e700758bcf6394536925c7b26a
Carlos Gonzalez <carlosegl10@hotmail.com>
2020-03-09 14:31:31 -0400

    Habilitada edicion de Posicion en el formulario

--- trackerApp/static/jquery/profile.js
+++ trackerApp/static/jquery/profile.js
@@ -1,6 +1,7 @@
 function edit() {
     document.getElementById("exampleInputEmail1").readOnly = false;
     document.getElementById("exampleInputPassword1").readOnly = false;
+    document.getElementById("exampleInputPassword2").readOnly = false;
     document.getElementById("button-gender").disabled = false;
     document.getElementById("button-save").hidden = false;
 }
@@ -8,6 +9,7 @@ function edit() {
 function save(){
     document.getElementById("exampleInputEmail1").readOnly = true;
     document.getElementById("exampleInputPassword1").readOnly = true;
+    document.getElementById("exampleInputPassword2").readOnly = true;
     document.getElementById("button-gender").disabled = true;
     document.getElementById("button-save").hidden = true;
 }
