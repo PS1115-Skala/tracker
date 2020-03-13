d2dfaa68a7a8873e1ea17e46d334b2c7004f18b4
alvaro <almiavicas@gmail.com>
2020-03-12 22:30:16 -0400

    refactored code

--- trackerApp/views.py
+++ trackerApp/views.py
@@ -21,11 +21,13 @@ class IndexView(LoginRequiredMixin, generic.list.ListView):
     model = Activity
     context_object_name = 'my_activities'
 
+
 class RedirectView(generic.base.RedirectView):
     pattern_name = 'login'
 
     def get_redirect_url(self, *args, **kwargs):
         return super().get_redirect_url(*args, **kwargs)
+
 
 class LoginView(generic.edit.FormView):
     form_class = LoginForm
@@ -36,7 +38,7 @@ class LoginView(generic.edit.FormView):
         form = self.form_class()
         context['form'] = form
         return context
-    
+
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
@@ -47,16 +49,15 @@ class LoginView(generic.edit.FormView):
                 login(request, user)
                 return HttpResponseRedirect('/index/')
             else:
-                context = {'form':form}
+                context = {'form': form}
                 messages.info(request, 'Correo o contraseña inválido')
-                return render(request, self.template_name, {'form':form})
+                return render(request, self.template_name, {'form': form})
         else:
-            context = {'form':form}
+            context = {'form': form}
             messages.info(request, 'Correo o contraseña inválido')
             return render(request, self.template_name, context)
 
-        
-        return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form': form})
 
 
 class RegisterView(generic.base.TemplateView):
@@ -85,84 +86,77 @@ class RegisterView(generic.base.TemplateView):
                 position = UserData(position=position, id_user=user)
                 position.save()
 
-
                 user = authenticate(request, username=email, password=password)
                 if user is not None:
                     login(request, user)
                     return HttpResponseRedirect('/index/')
                 else:
-                    context = {'form':form}
+                    context = {'form': form}
                     messages.info(request, 'Error de Servidor')
                     return render(request, self.template_name, context)
             except:
-                context = {'form':form}
+                context = {'form': form}
                 messages.info(request, 'Usuario Resgistrado')
                 return render(request, self.template_name, context)
-        context = {'form':form}
+        context = {'form': form}
         messages.info(request, 'Datos inválidos')
-        return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form': form})
+
 
 class ProfileView(generic.detail.DetailView):
-   
-    
+
     model = User
-    template_name = "trackerApp/base_profile.html"  
-
-    
-
+    template_name = "trackerApp/base_profile.html"
 
     def get(self, request, *args, **kwargs):
-        userdata=UserData.objects.get(id_user=request.user)
-        media_root=getattr(settings, 'MEDIA_ROOT', None)
-        # userdata=UserData.objects.get(id_user=request.user)          
-        request.session['position']=userdata.position
-        request.session['description']=userdata.description
-        request.session['image']=userdata.profileImage.url
-        request.session['genre']=userdata.genre
+        userdata = UserData.objects.get(id_user=request.user)
+        media_root = getattr(settings, 'MEDIA_ROOT', None)
+        request.session['position'] = userdata.position
+        request.session['description'] = userdata.description
+        request.session['image'] = userdata.profileImage.url
+        request.session['genre'] = userdata.genre
         return render(request, self.template_name)
 
-
     def post(self, request, *args, **kwargs):
-        validator=EmailValidator()
+        validator = EmailValidator()
 
-        userdata=UserData.objects.get(id_user=request.user.id)        
-        request.session['position']=userdata.position
-        request.session['description']=userdata.description  
-        email=request.POST['formInputEmail']
-        inputPosition=request.POST['formInputPosition']
-        inputDescription=request.POST['formInputDescription']
-        inputGenre=request.POST['formInputGenre']
-        inputImage=request.FILES.get('image')   
+        userdata = UserData.objects.get(id_user=request.user.id)
+        request.session['position'] = userdata.position
+        request.session['description'] = userdata.description
+        email = request.POST['formInputEmail']
+        inputPosition = request.POST['formInputPosition']
+        inputDescription = request.POST['formInputDescription']
+        inputGenre = request.POST['formInputGenre']
+        inputImage = request.FILES.get('image')
 
-        #request.session['genre']=user.genre
-        
-        #inputGenre=request.POST['formInputGenre']
+        # request.session['genre']=user.genre
+
+        # inputGenre=request.POST['formInputGenre']
 
         try:
-            if(inputPosition != ""): #Se introduce el cargo
-                userdata.position=inputPosition
-                userdata.save()             
-
-            if(inputGenre != ""): #Se introduce el cargo
-                userdata.genre=inputGenre
+            if(inputPosition != ""):  # Se introduce el cargo
+                userdata.position = inputPosition
                 userdata.save()
-            if(inputImage!=None and userdata.imageVerification()):
+
+            if(inputGenre != ""):  # Se introduce el cargo
+                userdata.genre = inputGenre
+                userdata.save()
+            if(inputImage != None and userdata.imageVerification()):
 
                 userdata.eraseOldMedia()
-                userdata.profileImage=inputImage
+                userdata.profileImage = inputImage
                 userdata.save()
 
-            
-            if(inputDescription!=""): #Se introduce la descripcion                
-                userdata.description=inputDescription
-                userdata.save() 
+            if(inputDescription != ""):  # Se introduce la descripcion
+                userdata.description = inputDescription
+                userdata.save()
 
-            validator(email) #Se verifica que se haya introducido un email
-            user=User.objects.get(id=request.user.id)
-            user.username=email
-            user.email=email
-            user.save()       
-            
+            validator(email)  # Se verifica que se haya introducido un email
+            user = User.objects.get(id=request.user.id)
+            user.username = email
+            user.email = email
+            user.save()
+
         except:
             return HttpResponseRedirect('/profile/')
 
@@ -173,8 +167,7 @@ class ActivityView(generic.detail.DetailView):
     model = User
     template_name = "trackerApp/activity.html"
 
+
 class LoanView(generic.detail.DetailView):
     model = User
     template_name = "trackerApp/base_loan.html"
-
-
