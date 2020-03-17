65b7bf7d4c34c205be012b36dfb22181dded069f
Carlos Gonzalez <carlosegl10@hotmail.com>
2020-03-16 22:26:19 -0400

    Corregida la descripcion, ahora muestra la descripcion actual para edicion

--- trackerApp/static/jquery/profile.js
+++ trackerApp/static/jquery/profile.js
@@ -3,14 +3,14 @@ function edit() {
     document.getElementById("exampleInputPassword1").readOnly = false;    
     document.getElementById("button-gender").disabled = false;
     document.getElementById("button-save").hidden = false;
-    document.getElementById("exampleInputDescription").readOnly = false;
+    document.getElementById("exampleInputDescription").disabled = false;
     document.getElementById("id_image").disabled = false;
 }
 
 function save(){
     document.getElementById("exampleInputEmail1").readOnly = true;
     document.getElementById("exampleInputPassword1").readOnly = true;    
-    document.getElementById("exampleInputDescription").readOnly = true;
+    document.getElementById("exampleInputDescription").disabled = true;
     document.getElementById("button-gender").disabled = true;
     document.getElementById("button-save").hidden = true;
     document.getElementById("id_image").disabled = true;
